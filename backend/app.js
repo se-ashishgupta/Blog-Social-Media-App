5ba27c71a140877c75d6ddbfe002ee8897f5594d
Ashish Gupta <gashish4950@gmail.com>
2023-01-22 11:54:26 +0530

    backend update commit

--- backend/app.js
+++ backend/app.js
@@ -18,15 +18,14 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(cors());
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     optionSuccessStatus: 200,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    optionSuccessStatus: 200,
+  })
+);
 
 //Importing & Using Routes
 import user from "./routes/userRoutes.js";
@@ -37,7 +36,7 @@ app.use("/api/v1", blog);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
