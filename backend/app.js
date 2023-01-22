9bc43b1c4ea17e9d103cb783d0b97983d0f29b29
Ashish Gupta <gashish4950@gmail.com>
2023-01-22 11:50:44 +0530

    backend update commit

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-// import cors from "cors";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
@@ -18,6 +18,7 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: process.env.FRONTEND_URL,
